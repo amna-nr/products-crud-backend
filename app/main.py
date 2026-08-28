18c15beeadaeb49999abac8122b35a6b32910e42
amna-nr <amnanoor5778@gmail.com>
2026-08-28 20:06:50 +0500

    feat: cors for frontend requests

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from app.routes.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
 app.include_router(auth_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def homepage():
