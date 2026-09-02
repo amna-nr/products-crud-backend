f8be2cef7ba8b55ceaaa1df0bb4e1baa374ed577
amna-nr <amnanoor5778@gmail.com>
2026-09-02 14:11:55 +0500

    fix: add missing env variable to settings

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str 
     REDIS_URL: str
+    ENVIRONMENT: str
+    
 
     class Config():
         env_file = ".env"
